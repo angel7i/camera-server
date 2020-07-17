368b0bcd2ac995adfccf2f8219ea61fd4cae8049
Angel Esquivel <plussnoise@gmail.com>
2020-07-17 00:41:36 -0500

    Update server.py

--- server/server.py
+++ server/server.py
@@ -26,7 +26,7 @@ def process_image():
             label = classify_image(img_data)
             img_data.close()
             resp["msg"] = label
-            resp["size"] = img_data["size"]
+            #resp["size"] = img_data["size"]
             #resp["data"] = img_data["data"]
 
     return resp
